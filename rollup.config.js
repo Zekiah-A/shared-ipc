df3bda827ed6fe7366cae7d633b45bf8ccce676d
Zekiah-A <zekiahamoako@outlook.com>
2025-04-13 17:29:08 +0100

    Add type definitions

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,13 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
 
 export default {
 	input: "src/index.js",
 	output: {
 		file: "dist/index.js",
 		format: "umd",
-		name: "SimpleIPC"
+		name: "shared-ipc"
 	},
 	plugins: [
-		nodeResolve(),
-		typescript({
-			declaration: true,
-			declarationDir: "dist",
-			allowJs: true,
-			checkJs: true
-		})
+		nodeResolve()
 	]
 };
